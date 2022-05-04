6a2f70700029450c8ecd11dfc3012a5ae939ed9d
robandrade <103383943+robandrade@users.noreply.github.com>
2022-05-04 18:32:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+StreamWriter outputFile;
+outputFile = File.CreateText("Activity12.txt");
+
+outputFile.WriteLine("Don't be late, teatime will not wait!");
+
+outputFile.Close();
+
+string str = File.ReadAllText("Activity12.txt");
+
+int word = 0;
+int last = 0;
+
+while (last < str.Length -1)
+{
+    if ((str[last] == ',' || str[last] == ' ' || str[last] == '!' || str[last] == '\t' || str[last] == '\n' || str[last] == '.' || str[last] == '?') && (str[last-1] == 't' || str[last-1] == 'e'))
+    {
+        word++;
+    }
+    last++;
+}
+
+Console.WriteLine("There are {0} words ending in t or e", word);
+ 
